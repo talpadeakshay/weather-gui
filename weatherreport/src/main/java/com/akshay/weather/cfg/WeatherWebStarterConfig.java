5a6d2acf5e46661467507f6f9bb47b421edafab8
Akshay <talpadeakshay@gmail.com>
2016-05-05 22:53:10 +1000

    Added Json Reader

--- weatherreport/src/main/java/com/akshay/weather/cfg/WeatherWebStarterConfig.java
+++ weatherreport/src/main/java/com/akshay/weather/cfg/WeatherWebStarterConfig.java
@@ -5,17 +5,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({ "com.akshay.weather" })
-public class WeatherWebStarterConfig {
+public class WeatherWebStarterConfig extends WebMvcConfigurerAdapter {
 	protected static final Logger LOGGER = LoggerFactory.getLogger(WeatherWebStarterConfig.class);
-		
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("forward:/index.html");
+	}
+
 	@Bean
-	public InternalResourceViewResolver viewResolver() {		
-		InternalResourceViewResolver viewResolver 
-                         = new InternalResourceViewResolver();		
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setPrefix("/WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
